71662a57b6d7d6b732ed01bac743eb6532fa1804
hawkinswinja <hawkinswinja@gmail.com>
2023-05-29 14:19:01 +0300

    add readme file

---
+++ README.md
@@ -0,0 +1,1 @@
+Complete service for file manager
--- utils/redis.js
+++ utils/redis.js
@@ -1,30 +1,31 @@
 import { createClient } from 'redis';
 import { promisify } from 'util';
-const client = createClient();
 
 class RedisClient {
   constructor() {
-    client.on('error', (e) => console.log(e));
+    this.client = createClient();
+    this.client.on('error', (e) => console.log(e));
   }
 
-	isAlive() {
-    return (client.on('connect', () => true) ? true : false);
-	}
+  isAlive() {
+    return (this.client.connected);
+  }
 
   async get(key) {
-    const get = promisify(client.get).bind(client);
-	  return await get(key);
+    const get = promisify(this.client.get).bind(this.client);
+    const val = await get(key);
+    return val;
   }
 
-	async set(key, val, time) {
-    await client.set(key, val);
-    await client.expire(key, time);
+  async set(key, val, time) {
+    await this.client.set(key, val);
+    await this.client.expire(key, time);
   }
 
-	async del(key) {
-    await client.del(key);
+  async del(key) {
+    await this.client.del(key);
   }
-};
+}
 
 const redisClient = new RedisClient();
 export default redisClient;
